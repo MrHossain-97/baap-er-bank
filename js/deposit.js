e5a26a8f602f4f07645a5a29a92281d2acb7c539
MrHossain97 <mrhossain97.programming@gmail.com>
2023-02-12 08:02:53 +0600

    deposit section added and updated new balance after deposit new amount

---
+++ js/deposit.js
@@ -0,0 +1,26 @@
+// step 1:
+document.getElementById('btn-deposit').addEventListener('click',function () {
+    // step 2:
+    const depositField = document.getElementById('deposit-amount');
+    // converted input value into float
+    const newDepositAmount = parseFloat(depositField.value);
+
+    // step 3:
+    // for not input we will use innerText to get the value
+    const totalDeposit = document.getElementById('total-deposit');
+    // converted deposit amount into float
+    const prevTotalDeposit = parseFloat(totalDeposit.innerText);
+    // add new deposit amount with previous deposit amount
+    const currentDepositTotal = prevTotalDeposit + newDepositAmount;
+    // print total deposit amount
+    totalDeposit.innerText = currentDepositTotal;
+
+    // set balance current total
+    const balance = document.getElementById('balance');
+    const prevBalance = parseFloat(balance.innerText);
+    // set new balance
+    const currentBalance = prevBalance + newDepositAmount;
+    balance.innerText = currentBalance;
+
+    document.getElementById('deposit-amount').value='';
+})
